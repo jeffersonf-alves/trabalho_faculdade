99ebb0149e48b42033bef6f2457bd74f1827c14c
Jefferson Alves <jefff9910@outlook.com>
2021-11-23 23:26:20 -0300

    Site pronto

---
+++ script.js
@@ -0,0 +1,15 @@
+function chamarPagina01() {
+    window.location.href = 'album/album01.html';
+}
+function chamarPagina02() {
+    window.location.href = 'album/album02.html';
+}
+function chamarPagina03() {
+    window.location.href = 'album/album03.html';
+}
+function chamarPagina04() {
+    window.location.href = 'album/album04.html';
+}
+function chamarPagina05() {
+    window.location.href = 'album/album05.html';
+}
